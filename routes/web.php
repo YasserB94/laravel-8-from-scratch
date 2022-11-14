1e8a2149c56f9bc1609ff9d6c7dfe6cdd4f21ea2
Yasser Bal <yasserbal@icloud.com>
2022-11-14 16:13:13 +0100

    Added Category fetch to most routes

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,34 @@ use App\Models\{Post,Category,User};
 Route::get('/', function () {
 
     return view('posts',[
-        'posts'=>Post::latest()->get()//->Get the Categories here so the view does not make extra SQL xcalls
+        'posts'=>Post::latest()->get(),//->Get the Categories here so the view does not make extra SQL xcalls
         //'posts' => Post::all()
         // This results in extra queries for each post when it gets the category
+        'categories'=>Category::all()
     ]);
 });
 Route::get('posts/{post:slug}',function(Post $post){
    return view('post',[
-       'post'=>$post
+       'post'=>$post,
 //       'post' => Post::findOrFail($id) We can inject into our function to ommit this call
+
+
    ]);
 });
 
 Route::get('categories/{category:slug}',function (Category $category){
     return view('posts',[
-        'posts' => $category->posts
+        'posts' => $category->posts,
+            'currentCategory'=>$category,
+            'categories'=>Category::all()
         ]
     );
 });
 Route::get('authors/{author:username}',function (User $author){
     return view('posts',[
-        'posts' => $author->posts
+        'posts' => $author->posts,
+            'categories'=>Category::all()
+
         ]
     );
 });
