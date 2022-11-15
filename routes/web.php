c5938caa38dff46c63821e884e5e41d77790db35
Yasser Bal <yasserbal@icloud.com>
2022-11-15 07:42:24 +0100

    Moved Category param to controllers/component class

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\{Post,Category,User};
+use \App\Http\Controllers\{PostController,Controller};
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,42 +14,22 @@ use App\Models\{Post,Category,User};
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-    if(request('search')){
-        $posts->where('title','like','%'.request('search').'%')
-            ->orWhere('body','like','%'.request('search').'%');
-    }
+Route::get('/',[PostController::class,'index']);
+Route::get('posts/{post:slug}',[PostController::class,'show']);
 
-    return view('posts',[
-        'posts'=>$posts->get(),//->Get the Categories here so the view does not make extra SQL xcalls
-        //'posts' => Post::all()
-        // This results in extra queries for each post when it gets the category
-        'categories'=>Category::all()
-    ]);
-});
-Route::get('posts/{post:slug}',function(Post $post){
-   return view('post',[
-       'post'=>$post,
-//       'post' => Post::findOrFail($id) We can inject into our function to ommit this call
-
-
-   ]);
-});
-
-Route::get('categories/{category:slug}',function (Category $category){
-    return view('posts',[
-        'posts' => $category->posts,
-            'currentCategory'=>$category,
-            'categories'=>Category::all()
-        ]
-    );
-});
 Route::get('authors/{author:username}',function (User $author){
     return view('posts',[
-        'posts' => $author->posts,
-            'categories'=>Category::all()
+        'posts' => $author->posts
+            //'categories'=>Category::all()
 
         ]
     );
 });
+//Route::get('categories/{category:slug}',function (Category $category){
+//    return view('posts',[
+//        'posts' => $category->posts,
+//        'currentCategory'=>$category
+//            'categories'=>Category::all()
+//        ]
+//    );
+//});
