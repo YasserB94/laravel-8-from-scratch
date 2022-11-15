6bb4f3c90c216de8dc94fadd740960d637c6931a
YasserB94 <yasserbal@icloud.com>
2022-11-15 13:29:59 +0100

    Update - Now creates more posts to implement pagination Added Posts with hardcoded relations to improve query testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,11 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Category::truncate();
         Post::truncate();
-        Post::factory(3)->create();
+        Post::factory(5)->create();
         Post::factory(5)->create(['user_id'=>1]);
         Post::factory(5)->create(['category_id'=>1]);
+        Post::factory(5)->create(['user_id'=>2,'category_id'=>2]);
+        Post::factory(5)->create(['user_id'=>2,'category_id'=>4]);
+        Post::factory(5)->create(['user_id'=>1,'category_id'=>3]);
     }
 }
