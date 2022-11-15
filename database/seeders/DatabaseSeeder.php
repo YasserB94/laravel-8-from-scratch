cbfbb50454162491543bbaa43652ac65d4a8f678
YasserB94 <yasserbal@icloud.com>
2022-11-15 16:36:43 +0100

    Posts now have many to one relations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\{Category,User,Post};
+use App\Models\{Category, User, Post};
 
 use Illuminate\Database\Seeder;
 
@@ -21,11 +21,15 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Category::truncate();
         Post::truncate();
-        Post::factory(5)->create(['user_id'=>1,'category_id'=>4]);
-        Post::factory(5)->create(['user_id'=>2,'category_id'=>2]);
-        Post::factory(5)->create(['user_id'=>2,'category_id'=>4]);
-        Post::factory(5)->create(['user_id'=>1,'category_id'=>3]);
-        Post::factory(5)->create(['user_id'=>3,'category_id'=>3]);
-        Post::factory(5)->create(['user_id'=>3,'category_id'=>2]);
+        $posts = 100;
+        $categories = 8;
+        $users = 12;
+        Category::factory($categories)->create();
+        User::factory($users)->create();
+
+        for($i=0;$i<$posts;$i++){
+            Post::factory(1)->create(['user_id' => rand(1,$users),'category_id' => rand(1,$categories)]);
+        }
+
     }
 }
