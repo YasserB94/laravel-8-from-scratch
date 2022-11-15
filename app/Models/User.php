78b8749bb083719e9d9fb075a92f45e228c109ee
Yasser Bal <yasserbal@icloud.com>
2022-11-15 21:08:31 +0100

    Added passwordEncryption Password now in fillable array

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
         'password',
     ];
 
@@ -43,5 +44,8 @@ class User extends Authenticatable
     ];
     public function posts(){
         return $this->hasMany(Post::class);
+    }
+    public function setPasswordAttribute(string $password){
+        $this->attributes['password'] = bcrypt($password);
     }
 }
