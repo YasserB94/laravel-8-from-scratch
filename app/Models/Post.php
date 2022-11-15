f80bf7861f95132906562afa4f69d67bb047d8b1
Yasser Bal <yasserbal@icloud.com>
2022-11-15 07:39:26 +0100

    Improved Search queries

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,20 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
     protected $guarded = ['id'];//Do not allow this value to be updated from the App
-    protected $with = ['category','author'];//Always Come with these relationships(Prevent too many Database requests)
+    protected $with = ['category', 'author'];//Always Come with these relationships(Prevent too many Database requests)
+
+    public function scopeFilter(Builder $query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function (Builder $query, $search) {
+            $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+//        if($filters['search'] ?? false)
+//        $query->where('title','like','%'.request('search').'%')
+//            ->orWhere('body','like','%'.request('search').'%');
+        $query->when($filters['category'] ?? false, function (Builder $query, $category) {
+            //Pick any post that has a category
+            $query->whereHas('category', fn(Builder $query) => //Select the one where the slug is equal to the category given in the GET uri
+            $query->where('slug', $category)
+            );
+        });
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
-    public function author(){
-        return $this->belongsTo(User::class,'user_id');
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 
 }
