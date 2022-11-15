488a6e6496c3d13110e1652ad4ac59f82b1ebf6d
YasserB94 <yasserbal@icloud.com>
2022-11-15 14:18:36 +0100

    Added Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,20 +8,15 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::latest()->filter(request(['search','category','author']))->get();
         return view('posts.index',[
-            'posts'=>$posts,
-            //'posts' => Post::all()
-            // This results in extra queries for each post when it gets the category
-            //'categories'=>Category::all(),
-//            'currentCategory' =>Category::firstWhere('slug',request('category')),
+            'posts'=>Post::latest()->filter(request(['search','category','author']))
+                ->paginate(10),
         ]);
     }
     public function show(Post $post){
 
         return view('posts.show',[
             'post'=>$post,
-//       'post' => Post::findOrFail($id) We can inject into our function to ommit this call
         ]);
     }
 }
