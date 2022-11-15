6d35acef7b043d7a0199e28e484516c0dddb619a
YasserB94 <yasserbal@icloud.com>
2022-11-15 13:20:40 +0100

    Returns refactored posts views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,6 +18,7 @@ class PostController extends Controller
         ]);
     }
     public function show(Post $post){
+
         return view('posts.show',[
             'post'=>$post,
 //       'post' => Post::findOrFail($id) We can inject into our function to ommit this call
