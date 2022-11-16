ade285b66329efde63d87484505d3b6c756527c2
Yasser Bal <yasserbal@icloud.com>
2022-11-16 20:01:19 +0100

    Flashes now also display errors in red

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -13,3 +13,18 @@
         <p>{{session('success')}}</p>
     </div>
 @endif
+@if(session()->has('errors'))
+    <div
+        x-data="{show:true}"
+        x-init="setTimeout(()=> show = false,5000)"
+        x-show="show"
+        x-transition:enter="transition ease-out duration-1000"
+        x-transition:enter-start="opacity-0 animate-bounce scale-50"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-1000"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-50"
+        {{ $attributes->class(['fixed bottom-8 rounded-xl scale right-4 p-4 bg-red-500 text-white text-center']) }}>
+        <p>{{session('errors')->first()}}</p>
+    </div>
+@endif
