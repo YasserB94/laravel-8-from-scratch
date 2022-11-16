144e068d69c81f17b275ef624b60e7983017dcfa
YasserB94 <yasserbal@icloud.com>
2022-11-16 11:01:24 +0100

    Create view for comment-card

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,14 @@
+<article {{ $attributes->class(['p-4 rounded  bg-gray-100 border border-gray-200 hover:border-l-blue-500']) }}>
+    <header class="flex  space-x-2">
+        <img class="rounded-xl max-h-16 aspect-square shadow-xl" src="https://i.pravatar.cc/100" alt="User avatar">
+        <h3 class="text-xl text-blue-500 mt-auto">Username</h3>
+    </header>
+    <main class="mt-2 ml-8">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, aperiam beatae dignissimos eligendi enim eum
+        labore laboriosam magnam, natus necessitatibus, nesciunt nulla odio officiis sint tempora? Earum fuga illum
+        voluptates.
+    </main>
+    <footer class="text-right text-xs text-gray-500">
+        <time>Posted X ago</time>
+    </footer>
+</article>
