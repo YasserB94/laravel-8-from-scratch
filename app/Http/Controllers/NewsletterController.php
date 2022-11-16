c21e42b93b63a6c194761cabec8f4bfce044a8a8
Yasser Bal <yasserbal@icloud.com>
2022-11-16 20:00:56 +0100

    Now Single __invoke function controller

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -5,26 +5,23 @@ namespace App\Http\Controllers;
 use App\Services\Newsletter;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use MailchimpMarketing\ApiClient;
-
 class NewsletterController extends Controller
 {
-    //
-    public function __construct(private Newsletter $newsletter){
+    public function __construct(private Request $request){
 
     }
-    public function subscribe(Request $request){
-        $request->validate([
-            'email'=>['email','min:5']
-        ]);
+    public function __invoke(Newsletter $newsletter)
+    {
+        $this->request->validate(['email' => ['required', 'email','min:5']]);
         try {
-            $this->newsletter->subscribe($request->get('email'));
-        }catch (\Exception $e){
-            ValidationException::withMessages([
-                'email'=>'Oops! something went wrong. We are sorry'
+            $newsletter->subscribe($this->request->get('email'));
+        } catch (\Exception $e) {
+            throw ValidationException::withMessages([
+                'email' => 'This email could not be added to the newsletter, we\'re sorry'
             ]);
         }
-    return back()->with('success','Congratulations! You are now subscribed to the newsletter');
+        return back()->with('success', 'Congratulations! You are now subscribed to the newsletter');
     }
 
 }
+
