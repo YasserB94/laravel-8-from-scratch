6ea4ae936b1044110b0f86ad152dc514fb33c002
Yasser Bal <yasserbal@icloud.com>
2022-11-17 07:46:21 +0100

    Extracted commit button to component:

---
+++ resources/views/components/button-submit.blade.php
@@ -0,0 +1,4 @@
+<button
+        {{ $attributes->class(['bg-blue-400 block ml-auto border-2 border-gray-200 text-gray-100 hover:bg-gray-100 transition-all duration-150 hover:border-blue-500 hover:text-blue-700 px-4 py-2 rounded-xl text-center outline-none outline-0 focus:outline-gray-100 focus:shadow focus:text-blue-700 focus:bg-transparent']) }}>
+    {{$slot}}
+</button>
---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,2 @@
+    <div {{ $attributes->class(['border border-gray-200 p-6 rounded-xl hover:border-gray-100 shadow hover:shadow-none']) }}>
+    </div>
