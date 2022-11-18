675a15efb1599a7221c63d9630610ca01db60c18
YasserB94 <yasserbal@icloud.com>
2022-11-18 16:20:14 +0100

    Cleaned up Routes

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,14 @@ Route::post('subscribe',NewsletterController::class);
 Route::post('/posts/subscribe',NewsletterController::class);
 
 //ADMIN ROUTES
-Route::get('admin/posts/create',[AdminPostController::class,'create'])->middleware('admin');
-Route::get('admin/posts',[AdminPostController::class,'index'])->middleware('admin');
-Route::get('admin/posts/{post}/edit',[AdminPostController::class,'edit'])->middleware('admin');
-Route::post('admin/posts',[AdminPostController::class,'store'])->middleware('admin');
-Route::patch('admin/posts/{post}',[AdminPostController::class,'update'])->middleware('admin');
-Route::delete('admin/posts/{post}',[AdminPostController::class,'destroy'])->middleware('admin');
+Route::middleware('can:admin')->group(function(){
+    Route::resource('admin/posts',AdminPostController::class)->except('show');
+//    Route::get('admin/posts/create',[AdminPostController::class,'create']);//->middleware('admin');
+//    Route::get('admin/posts',[AdminPostController::class,'index']);//->middleware('admin');
+//    Route::get('admin/posts/{post}/edit',[AdminPostController::class,'edit']);//->middleware('admin');
+//    Route::post('admin/posts',[AdminPostController::class,'store']);//->middleware('admin');
+//    Route::patch('admin/posts/{post}',[AdminPostController::class,'update']);//->middleware('admin');
+//    Route::delete('admin/posts/{post}',[AdminPostController::class,'destroy']);//->middleware('admin');
+
+});
+
